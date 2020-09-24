32c077235a72a8723b36074cab9e83e9b8449b60
Renata300 <renatamottin357@gmail.com>
2020-09-24 20:12:59 -0300

    Colocando as opcoes do usuario

--- Sistema Bancario/src/test/java/com/renatam/ContaTest.java
+++ Sistema Bancario/src/test/java/com/renatam/ContaTest.java
@@ -4,44 +4,44 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-public class AppTest 
+public class ContaTest 
 {
     @Test
     public void saldoInicialBaixo()
     {
-        assertTrue( true );
+        Assertions.assertEqual(500, saldo, categoria);
     }
     @Test
     public void saldoInicialMedio()
     {
-        assertTrue( true );
+        Assertions.assertEqual(51000, saldo, categoria);
     }
     @Test
     public void saldoInicialAlto()
     {
-        assertTrue( true );
+        Assertions.assertEqual(250000, saldo, categoria);
     }
     @Test
     public void siverGold()
     {
-        assertTrue( true );
+        Assertions.assertEqual(50000, saldo, categoria);
     }
 
     @Test
     public void goldPlatinum()
     {
-        assertTrue( true );
+        Assertions.assertEqual(200000, saldo, categoria);
     }
 
     @Test
     public void platinumGold()
     {
-        assertTrue( true );
+        Assertions.assertEqual(90000, saldo, categoria);
     }
 
     @Test
     public void goldSilver()
     {
-        assertTrue( true );
+        Assertions.assertEqual(24999, saldo, categoria);
     }
 }
