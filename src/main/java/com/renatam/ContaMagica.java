24dce80f3a474a6799c1244043d766bf2afb4b50
Luiz Carvalho <luiz.ferreira.carvalho@hotmail.com>
2020-09-24 22:01:41 -0300

    Add contaTeste tests

--- src/main/java/com/renatam/ContaMagica.java
+++ src/main/java/com/renatam/ContaMagica.java
@@ -4,6 +4,24 @@ public class ContaMagica
 {
     public static void main( String[] args)
     {
-        System.out.println("Hello");
+        ContaCorrente cc = new ContaCorrente("a", "b");
+
+        cc.deposito(200000);
+        printConta(cc);
+        cc.deposito(200000);
+        printConta(cc);
+        cc.retirada(390000);
+        printConta(cc);
+        cc.retirada(1);
+        printConta(cc);
+
+    }
+    
+    private static void printConta(ContaCorrente cc) {  
+        System.out.println("-----------------");
+        System.out.println("Conta:");
+        System.out.println(cc.getSaldo());
+        System.out.println(cc.getCategoria());
+        System.out.println("-----------------");
     }
 }
--- src/test/java/com/renatam/ContaTest.java
+++ src/test/java/com/renatam/ContaTest.java
@@ -1,47 +1,61 @@
 package com.renatam;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.Before;
+import org.junit.Assert;
 
 public class ContaTest 
 {
-    @Test
-    public void saldoInicialBaixo()
-    {
-        Assertions.assertEqual(500, saldo, categoria);
-    }
-    @Test
-    public void saldoInicialMedio()
-    {
-        Assertions.assertEqual(51000, saldo, categoria);
-    }
-    @Test
-    public void saldoInicialAlto()
-    {
-        Assertions.assertEqual(250000, saldo, categoria);
-    }
-    @Test
-    public void siverGold()
-    {
-        Assertions.assertEqual(50000, saldo, categoria);
+    private ContaCorrente cc;
+
+    @Before
+    public void initialize() {
+        String nome = "Nome Usuario";
+        String numeroConta = "123456";
+        cc = new ContaCorrente(numeroConta, nome);
     }
 
     @Test
-    public void goldPlatinum()
+    public void newAccountShouldStartInSilverAndWithNoBalance()
     {
-        Assertions.assertEqual(200000, saldo, categoria);
-    }
+        var expectedBalance = 0.0;
+        var expectedCategory = Categoria.SILVER;
 
-    @Test
-    public void platinumGold()
-    {
-        Assertions.assertEqual(90000, saldo, categoria);
+        Assert.assertEquals(expectedBalance, cc.getSaldo(), 0.001);
     }
+//     @Test
+//     public void saldoInicialMedio()
+//     {
+//         Assertions.assertEqual(51000, saldo, categoria);
+//     }
+//     @Test
+//     public void saldoInicialAlto()
+//     {
+//         Assertions.assertEqual(250000, saldo, categoria);
+//     }
+//     @Test
+//     public void siverGold()
+//     {
+//         Assertions.assertEqual(50000, saldo, categoria);
+//     }
 
-    @Test
-    public void goldSilver()
-    {
-        Assertions.assertEqual(24999, saldo, categoria);
-    }
+//     @Test
+//     public void goldPlatinum()
+//     {
+//         Assertions.assertEqual(200000, saldo, categoria);
+//     }
+
+//     @Test
+//     public void platinumGold()
+//     {
+//         Assertions.assertEqual(90000, saldo, categoria);
+//     }
+
+//     @Test
+//     public void goldSilver()
+//     {
+//         Assertions.assertEqual(24999, saldo, categoria);
+//     }
 }
