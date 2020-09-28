10fd7bbb75e35ded464ec75b1b54ab17d15133ee
Luiz Carvalho <luiz.ferreira.carvalho@hotmail.com>
2020-09-27 21:58:37 -0300

    Change input numbers

--- src/test/java/com/renatam/ContaTest.java
+++ src/test/java/com/renatam/ContaTest.java
@@ -19,7 +19,7 @@ public class ContaTest
         cc = new ContaCorrente(numeroConta, nome);
     }
 
-
+    
     @Test
     public void newAccountShouldStartInSilverAndWithNoBalance()
     {
@@ -27,11 +27,18 @@ public class ContaTest
         var expectedCategory = Categoria.SILVER;
         var expectedName = "Nome Usuario";
         var expectedCountNumber = "123456";
-
+        
         Assert.assertEquals(expectedBalance, cc.getSaldo(), 0.001);
         Assert.assertEquals(expectedCategory, cc.getCategoria());
         Assert.assertEquals(expectedCountNumber, cc.getNumeroConta());
         Assert.assertEquals(expectedName, cc.getNomeCorrentista());
+    }
+    
+    @Test
+    public void depositNegativeValueShouldReturnFalse() {
+        var depositResult = cc.deposito(-1);
+
+        Assert.assertFalse(depositResult);
     }
 
     @Test
@@ -40,9 +47,19 @@ public class ContaTest
         Assert.assertTrue(cc.deposito(500));
         Assert.assertTrue(cc.retirada(200));
     }
+
+    @Test
+    public void categoryShouldRemainSilverAfterDeposit()
+    {
+        cc.deposito(49999);
+        var categoriaEsperada = Categoria.SILVER;
+        var categoriaAtual = cc.getCategoria();
+
+        Assert.assertEquals(categoriaEsperada, categoriaAtual);
+    }
     
     @Test
-    public void covertSilverToGold() //quando passa de silver para gold
+    public void covertSilverToGold()
     {
         cc.deposito(50000);
         var categoriaEsperada = Categoria.GOLD;
@@ -62,7 +79,7 @@ public class ContaTest
     }
 
     @Test
-    public void convertGoldToPlatinum() //quando passa de gold para platinum
+    public void convertGoldToPlatinum()
     {
         cc.deposito(50000);
         cc.deposito(150000);
@@ -73,11 +90,11 @@ public class ContaTest
     }
 
     @Test
-    public void convertPlatinumToGold() //quando passa de platinum para gold
+    public void convertPlatinumToGold()
     {
         cc.deposito(50000);
         cc.deposito(150000);
-        cc.retirada(120000);
+        cc.retirada(103751);
         var categoriaEsperada = Categoria.GOLD;
         var categoriaAtual = cc.getCategoria();
 
@@ -85,11 +102,11 @@ public class ContaTest
     }
 
     @Test
-    public void notConvertPlatinumToSilver() //verificar se pula
+    public void notConvertPlatinumToSilver()
     {
         cc.deposito(50000);
         cc.deposito(150000);
-        cc.retirada(190000);
+        cc.retirada(178751);
         var categoriaEsperada = Categoria.GOLD;
         var categoriaAtual = cc.getCategoria();
 
@@ -97,7 +114,7 @@ public class ContaTest
     }
 
     @Test
-    public void convertGoldToSilver() //quando passa de gold para silver
+    public void convertGoldToSilver()
     {
         cc.deposito(50000);
         cc.retirada(25001);
@@ -111,7 +128,7 @@ public class ContaTest
     public void retiraMaisDoQuePossui()
     {
         cc.deposito(50000);
-        var resultadoRetirada = cc.retirada(100000);
+        var resultadoRetirada = cc.retirada(50001);
 
         Assert.assertFalse(resultadoRetirada);
     }
